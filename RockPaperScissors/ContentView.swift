4dc0fc0dd1a4d2bcf666bd25dd286f240d88107d
MMTabaka <miloszmtabaka@gmail.com>
2022-06-23 15:40:47 +0100

    Basic UI completed

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -7,10 +7,72 @@
 
 import SwiftUI
 
+struct HandIconModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(width: 100, height: 100)
+            .background(Circle().fill(.white))
+            .font(.system(size: 75))
+    }
+}
+
+extension View {
+    func handIcon() -> some View {
+        modifier(HandIconModifier())
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                Text("Rock Paper Scissors")
+                    .fontWeight(.bold)
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding()
+                Spacer()
+                VStack {
+                    Text("Win")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .fontWeight(.heavy)
+                    Text("👊")
+                        .handIcon()
+                }
+                Spacer()
+                Spacer()
+                Spacer()
+                HStack(spacing: 5) {
+                    Button {
+                        // action
+                    } label: {
+                        Text("👊")
+                            .handIcon()
+                    }
+                    
+                    Button {
+                        // action
+                    } label: {
+                        Text("🤚")
+                            .handIcon()
+                    }
+                    
+                    Button {
+                        // action
+                    } label: {
+                        Text("✌️")
+                            .handIcon()
+                    }
+                }
+                .padding()
+                
+                Spacer()
+            }
+        }
     }
 }
 
