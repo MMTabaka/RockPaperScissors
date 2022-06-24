64ad81a1b13087ea32d668f78adf962879b2506d
MMTabaka <miloszmtabaka@gmail.com>
2022-06-24 02:03:41 +0100

    Project completed

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -12,8 +12,7 @@ struct HandIconModifier: ViewModifier {
     func body(content: Content) -> some View {
         content
             .frame(width: 100, height: 100)
-            .background(Circle().fill(.white))
-            .font(.system(size: 75))
+            .font(.system(size: 90))
     }
 }
 
@@ -26,10 +25,14 @@ extension View {
 struct ContentView: View {
     @State private var mode: Bool = true
     @State private var showingResult = false
+    @State private var showingEnd = false
     let rules = ["👊": "🤚", "🤚": "✌️", "✌️": "👊"]
     let moves = ["👊", "🤚", "✌️"]
     @State private var opponent = Int.random(in: 0...2)
     @State private var scoreTitle = ""
+    @State private var score = 0
+    let numOfGames = 4
+    @State private var currentGame = 1
     
     
     
@@ -37,6 +40,12 @@ struct ContentView: View {
         ZStack {
             Color.blue
                 .ignoresSafeArea()
+            
+            Text(currentGame, format: .number)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                .padding()
+                .foregroundColor(.white)
+            
             VStack {
                 Spacer()
                 Text("Rock Paper Scissors")
@@ -52,17 +61,27 @@ struct ContentView: View {
                         .fontWeight(.heavy)
                     Text(moves[opponent])
                         .handIcon()
+                        .frame(width: 110 , height: 110)
+                        .background(.white)
+                        .clipShape(Circle())
                 }
                 Spacer()
                 Spacer()
+                
+                Text("\(score)/\(numOfGames)")
+                    .font(.title2)
+                    .foregroundColor(.white)
+                
                 Spacer()
-                HStack(spacing: 5) {
+                
+                HStack(spacing: 20) {
                     ForEach(moves, id: \.self) { move in
                         Button {
                             moveTapped(move)
                         } label: {
                             Text(move)
                                 .handIcon()
+                                .shadow(color: .black, radius: 20)
                         }
                     }
                 }
@@ -76,24 +95,48 @@ struct ContentView: View {
                 play()
             }
         }
+        .alert(scoreTitle, isPresented: $showingEnd) {
+            Button("Play again") {
+                reset()
+            }
+        } message: {
+            Text("Your score: \(score)/\(numOfGames)")
+        }
+        
     }
     
     func moveTapped(_ move: String) {
         let opponentMove = moves[opponent]
         
         if mode {
-            scoreTitle = rules[opponentMove] == move ? "Correct" : "Incorrect"
+            scoreTitle = rules[opponentMove] == move ? "Correct" : "Incorrect. The answer was \(move)"
         } else {
-            scoreTitle = rules[move] == opponentMove ? "Correct" : "Incorrect"
+            scoreTitle = rules[move] == opponentMove ? "Correct" : "Incorrect. The answer was \(move)"
         }
         
-        print(opponentMove)
-        showingResult = true
+        if scoreTitle == "Correct" {
+            score += 1
+        }
+        
+        
+        if currentGame == numOfGames {
+            showingEnd = true
+        } else {
+            showingResult = true
+        }
     }
     
     func play() {
+        currentGame += 1
         mode.toggle()
         opponent = Int.random(in: 0...2)
+    }
+    
+    func reset() {
+        mode = true
+        opponent = Int.random(in: 0...2)
+        currentGame = 1
+        score = 0
     }
 }
 
